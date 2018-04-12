183ab1edb5c63781e2b9bf44833a428c906c2488
katcarline <katie.carline@gmail.com>
2018-04-12 17:16:54 -0400

    searchbar for product

--- assets/js/app.js
+++ assets/js/app.js
@@ -518,7 +518,7 @@ $(document).one("ajaxStop", function () {
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     remote: {
-      url:"http://api.geonames.org/searchJSON?username=carlinek&featureClass=P&maxRows=5&countryCode=SA&name_startsWith=%QUERY" 
+      url:"http://api.geonames.org/searchJSON?username=carlinek&featureClass=P&maxRows=5&countryCode=SA&product_startsWith=%QUERY" 
         /*"http://api.geonames.org/searchJSON?username=bootleaf&featureClass=P&maxRows=5&countryCode=US&name_startsWith=%QUERY"*/,
       filter: function (data) {
         return $.map(data.geonames, function (result) {
